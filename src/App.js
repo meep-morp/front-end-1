92bca911c1b157b55229e80275bbf655faa9d91c
meep-morp <gracieaerogirl@gmail.com>
2020-07-31 09:48:34 -0500

    Added nav bar to home screen

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import Register from './Register'
-import Login from './Login';
-import { Route, Link } from 'react-router-dom';
+import React from "react";
+import Register from "./Register";
+import Login from "./Login";
+import { Route, Link } from "react-router-dom";
 // import { Button } from 'reactstrap';
-import './App.css';
-import PrivateRoute from './utils/PrivateRoute';
-import Dashboard from './Dashboard/Dashboard';
-import { AppProvider } from './context/AppContext';
+import "./App.css";
+import PrivateRoute from "./utils/PrivateRoute";
+import Dashboard from "./Dashboard/Dashboard";
+import { AppProvider } from "./context/AppContext";
+import Nav from "./Nav";
 
 function App() {
-
-  return (
-
-    <div className='landbuttons'>
-      {/* <AppProvider> */}
-      <Route exact path='/register'>
-        <Link className='RegLink' to='/login'>
-          <button color="primary" size="lg">Already a user?</button>
-        </Link>
-        <Register />
-      </Route>
-      <Route exact path='/login'>
-        <Link className='RegLink' to='/register'>
-          <button color="primary" size="lg"> Register Here </button>
-        </Link>
-        <Login />
-      </Route>
-      <PrivateRoute path="/dashboard">
-        <Dashboard />
-      </PrivateRoute>
-      {/* </AppProvider> */}
-    </div>
-
-  )
+	return (
+		<>
+			<Nav />
+			<div className="landbuttons">
+				<Route exact path="/register">
+					<Link className="RegLink" to="/login">
+						<button color="primary" size="lg">
+							Already a user?
+						</button>
+					</Link>
+					<Register />
+				</Route>
+				<Route exact path="/login">
+					<Link className="RegLink" to="/register">
+						<button color="primary" size="lg">
+							{" "}
+							Register Here{" "}
+						</button>
+					</Link>
+					<Login />
+				</Route>
+				<PrivateRoute path="/dashboard">
+					<Dashboard />
+				</PrivateRoute>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = (props) => {
+    return (
+        <nav id="nav">
+            <a href="/login">Login</a>
+            <a href="/register">Signup</a>
+            <a href="https://landbnb.netlify.app/">Home</a>
+        </nav>
+    )
+}
+
+export default Nav;
